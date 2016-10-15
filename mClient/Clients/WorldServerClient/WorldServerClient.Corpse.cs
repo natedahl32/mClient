e27ba5e5531805ed9da799f0085c1049284aa3f0
natedahl32 <nndahl@gmail.com>
2016-10-15 09:12:16 -0500

    Adding some missing files

---
+++ mClient/Clients/WorldServerClient/WorldServerClient.Corpse.cs
@@ -0,0 +1,81 @@
+﻿using mClient.Constants;
+using mClient.Network;
+using mClient.Shared;
+
+namespace mClient.Clients
+{
+    public partial class WorldServerClient
+    {
+        #region Handler Methods
+
+        /// <summary>
+        /// Handles corpse query
+        /// </summary>
+        /// <param name="packet"></param>
+        [PacketHandlerAtribute(WorldServerOpCode.MSG_CORPSE_QUERY)]
+        public void HandleCorpseQuery(PacketIn packet)
+        {
+            var isFound = packet.ReadByte();
+            if (isFound == 0)
+                return;
+
+            // corpse was found
+            var mapId = packet.ReadUInt32();
+            var corpseLocation = packet.ReadCoords3();
+
+            // teleport to our corpse
+            var newPosition = new Coordinate(corpseLocation.X, corpseLocation.Y, corpseLocation.Z);
+            Teleport(mapId, newPosition);
+            player.PlayerObject.Position = newPosition;
+        }
+
+        /// <summary>
+        /// Handles a resurrect request
+        /// </summary>
+        /// <param name="packet"></param>
+        [PacketHandlerAtribute(WorldServerOpCode.SMSG_RESURRECT_REQUEST)]
+        public void HandleResurrectRequest(PacketIn packet)
+        {
+            var resserGuid = packet.ReadUInt64();
+            // If the resser is in our party then accept it
+            if (player.CurrentGroup.IsInGroup(resserGuid))
+                AcceptResurrectRequest();
+        }
+
+        #endregion
+
+        #region Send Methods
+
+        /// <summary>
+        /// Sends request to reclaim our corpse
+        /// </summary>
+        public void ReclaimCorpse()
+        {
+            PacketOut packet = new PacketOut(WorldServerOpCode.CMSG_RECLAIM_CORPSE);
+            packet.Write(player.Guid.GetOldGuid());
+            Send(packet);
+        }
+
+        /// <summary>
+        /// Accepts a resurrect request
+        /// </summary>
+        public void AcceptResurrectRequest()
+        {
+            PacketOut packet = new PacketOut(WorldServerOpCode.CMSG_RESURRECT_RESPONSE);
+            packet.Write(player.Guid.GetOldGuid());
+            packet.Write(1);
+            Send(packet);
+        }
+
+        /// <summary>
+        /// Sends query for our corpse
+        /// </summary>
+        public void SendCorpseQuery()
+        {
+            PacketOut packet = new PacketOut(WorldServerOpCode.MSG_CORPSE_QUERY);
+            Send(packet);
+        }
+
+        #endregion
+    }
+}
---
+++ mClient/World/AI/PlayerAI.Death.cs
@@ -0,0 +1,52 @@
+﻿using FluentBehaviourTree;
+
+namespace mClient.World.AI
+{
+    public partial class PlayerAI
+    {
+        /// <summary>
+        /// Gets or sets whether or not we are teleporting to our corpse
+        /// </summary>
+        public bool IsTeleportingToCorpse { get; set; }
+
+        protected IBehaviourTreeNode CreateDeathAITree()
+        {
+            // TODO: Need to code in a wait for rez if we are in combat
+            var builder = new BehaviourTreeBuilder();
+            return builder
+                .Sequence("death-selector")
+                    .Do("Is Dead?", t =>
+                    {
+                        if (Player.PlayerObject.IsDead)
+                            return BehaviourTreeStatus.Success;
+                        return BehaviourTreeStatus.Failure;
+                    })
+                    .Do("Do I have a corpse?", t =>
+                    {
+                        if (Player.PlayerCorpse == null)
+                        {
+                            if (!IsTeleportingToCorpse)
+                            {
+                                Client.SendCorpseQuery();
+                                IsTeleportingToCorpse = true;
+                                return BehaviourTreeStatus.Running;
+                            }
+                            else
+                                return BehaviourTreeStatus.Running;
+                        }
+
+                        // We have our corpse now
+                        IsTeleportingToCorpse = false;
+                        return BehaviourTreeStatus.Success;
+                    })
+                    .Do("Release spirit!!", t =>
+                    {
+                        Client.ReclaimCorpse();
+                        Player.ResurrectFromDeath();
+                        return BehaviourTreeStatus.Success;
+                    })
+                 .End()
+                 .Build();
+        }
+    }
+}
