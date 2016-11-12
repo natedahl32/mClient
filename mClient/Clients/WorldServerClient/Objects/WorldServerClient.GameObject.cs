188e2eb6c81ed02b24874795300c87939751c5a6
Nate Dahl <nndahl@gmail.com>
2016-11-12 14:30:33 -0600

    Added flags for game objects that will help determine if we can interact with them or not

--- mClient/Clients/WorldServerClient/Objects/WorldServerClient.GameObject.cs
+++ mClient/Clients/WorldServerClient/Objects/WorldServerClient.GameObject.cs
@@ -1,4 +1,5 @@
-﻿using mClient.Shared;
+﻿using mClient.Constants;
+using mClient.Shared;
 using mClient.World.GameObject;
 using System;
 using System.Collections.Generic;
@@ -27,6 +28,72 @@ namespace mClient.Clients
         /// Gets or sets whether or not this game object has been looted (if it is lootable)
         /// </summary>
         public bool HasBeenLooted { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether or not we can interact with this game object
+        /// </summary>
+        public bool CanInteract
+        {
+            get
+            {
+                var value = (GameObjectFlags)GetFieldValue((int)GameObjectFields.GAMEOBJECT_FLAGS);
+                // If the value has the NO_INTERACT flag we cannot interact
+                if (value.HasFlag(GameObjectFlags.GO_FLAG_NO_INTERACT) || value.HasFlag(GameObjectFlags.GO_FLAG_INTERACT_COND))
+                    return false;
+
+                // Questgivers and chests have a dynamic flag set that determines if we can interact
+                if (BaseInfo.GameObjectType == GameObjectType.Chest || BaseInfo.GameObjectType == GameObjectType.QuestGiver)
+                {
+                    var dynValue = (GameObjectDynamicLowFlags)GetFieldValue((int)GameObjectFields.GAMEOBJECT_DYN_FLAGS);
+                    return dynValue.HasFlag(GameObjectDynamicLowFlags.GO_DYNFLAG_LO_ACTIVATE) || dynValue.HasFlag(GameObjectDynamicLowFlags.GO_DYNFLAG_LO_SPARKLE);
+                }
+                // Goobers also have the dynamic flag set but just use the activate flag, not the sparkle flag
+                if (BaseInfo.GameObjectType == GameObjectType.Goober)
+                {
+                    var dynValue = (GameObjectDynamicLowFlags)GetFieldValue((int)GameObjectFields.GAMEOBJECT_DYN_FLAGS);
+                    return dynValue.HasFlag(GameObjectDynamicLowFlags.GO_DYNFLAG_LO_ACTIVATE);
+                }
+
+                // No non-interact flag, so we assume we can interact
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Gets whether or not the game object is locked
+        /// </summary>
+        public bool IsLocked
+        {
+            get
+            {
+                var value = (GameObjectFlags)GetFieldValue((int)GameObjectFields.GAMEOBJECT_FLAGS);
+                return value.HasFlag(GameObjectFlags.GO_FLAG_LOCKED);
+            }
+        }
+
+        /// <summary>
+        /// Gets whether or not the game object is triggered
+        /// </summary>
+        public bool IsTriggered
+        {
+            get
+            {
+                var value = (GameObjectFlags)GetFieldValue((int)GameObjectFields.GAMEOBJECT_FLAGS);
+                return value.HasFlag(GameObjectFlags.GO_FLAG_TRIGGERED);
+            }
+        }
+
+        /// <summary>
+        /// Gets whether or not the game object is currently in use
+        /// </summary>
+        public bool IsInUse
+        {
+            get
+            {
+                var value = (GameObjectFlags)GetFieldValue((int)GameObjectFields.GAMEOBJECT_FLAGS);
+                return value.HasFlag(GameObjectFlags.GO_FLAG_IN_USE);
+            }
+        }
 
         #endregion
     }
--- mClient/Constants/Constants.Objects.cs
+++ mClient/Constants/Constants.Objects.cs
@@ -150,4 +150,23 @@ namespace mClient.Constants
         CapturePoint = 29,
         AuraGenerator = 30
     }
+
+    public enum GameObjectFlags
+    {
+        GO_FLAG_IN_USE = 0x00000001,                   // disables interaction while animated
+        GO_FLAG_LOCKED = 0x00000002,                   // require key, spell, event, etc to be opened. Makes "Locked" appear in tooltip
+        GO_FLAG_INTERACT_COND = 0x00000004,                   // cannot interact (condition to interact)
+        GO_FLAG_TRANSPORT = 0x00000008,                   // any kind of transport? Object can transport (elevator, boat, car)
+        GO_FLAG_NO_INTERACT = 0x00000010,                   // players cannot interact with this go (often need to remove flag in event)
+        GO_FLAG_NODESPAWN = 0x00000020,                   // never despawn, typically for doors, they just change state
+        GO_FLAG_TRIGGERED = 0x00000040                    // typically, summoned objects. Triggered by spell or other events
+    }
+
+    public enum GameObjectDynamicLowFlags
+    {
+        GO_DYNFLAG_LO_ACTIVATE = 0x01,                 // enables interaction with GO
+        GO_DYNFLAG_LO_ANIMATE = 0x02,                 // possibly more distinct animation of GO
+        GO_DYNFLAG_LO_NO_INTERACT = 0x04,                 // appears to disable interaction (not fully verified)
+        GO_DYNFLAG_LO_SPARKLE = 0x08,                 // makes GO sparkle TODO is it valid??
+    }
 }
