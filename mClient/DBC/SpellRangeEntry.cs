a6d86b69bf3311cb2a995652bb6a57b21991a4e3
natedahl32 <nndahl@gmail.com>
2016-11-16 23:48:33 -0600

    Added SpellRange dbc

---
+++ mClient/DBC/SpellRangeEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mClient.DBC
+{
+    public class SpellRangeEntry
+    {
+        #region Properties
+
+        public uint ID { get; set; }
+
+        public float MinimumRange { get; set; }
+
+        public float MaximumRange { get; set; }
+
+        #endregion
+    }
+}
---
+++ mClient/DBC/SpellRangeTable.cs
@@ -0,0 +1,40 @@
+﻿using mClient.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mClient.DBC
+{
+    public class SpellRangeTable : DBCFile
+    {
+        private Dictionary<uint, SpellRangeEntry> mSpellRangeEntries = new Dictionary<uint, SpellRangeEntry>();
+
+        #region Singleton
+
+        static readonly SpellRangeTable instance = new SpellRangeTable();
+
+        static SpellRangeTable() { }
+
+        SpellRangeTable() : base(@"SpellRange.dbc")
+        { }
+
+        public static SpellRangeTable Instance { get { return instance; } }
+
+        #endregion
+
+        protected override void dataLoaded()
+        {
+            for (uint i = 0; i < Records; i++)
+            {
+                var entry = new SpellRangeEntry();
+                entry.ID = getFieldAsUint32(i, 0);
+                entry.MinimumRange = getFieldAsFloat(i, 1);
+                entry.MaximumRange = getFieldAsFloat(i, 2);
+
+                mSpellRangeEntries.Add(entry.ID, entry);
+            }
+        }
+    }
+}
