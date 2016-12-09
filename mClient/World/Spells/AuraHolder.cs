6e551edb0b3968ae1fdb88e784c069dc14b49186
Nate Dahl <nndahl@gmail.com>
2016-12-08 23:35:42 -0600

    Added missing file

---
+++ mClient/World/Spells/AuraHolder.cs
@@ -0,0 +1,26 @@
+﻿using mClient.DBC;
+
+namespace mClient.World.Spells
+{
+    public class AuraHolder
+    {
+        #region Properties
+
+        /// <summary>
+        /// Get the spell for this aura
+        /// </summary>
+        public SpellEntry Spell { get; set; }
+
+        /// <summary>
+        /// Gets the duration of the aura in seconds
+        /// </summary>
+        public float Duration { get; set; }
+
+        /// <summary>
+        /// Gets the number of stacks of this aura
+        /// </summary>
+        public int Stacks { get; set; }
+
+        #endregion
+    }
+}
